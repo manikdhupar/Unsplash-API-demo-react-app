c19a6aa9fafab2cf7716e5e0b4ef1cf55b240d57
manik dhupar <manik.dhupar7@gmail.com>
2019-01-26 14:41:04 +0530

    added query and image component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Titles from './components/Titles';
 import './App.css';
 
 import axios from 'axios';
+import Unsplash from 'unsplash-js';
 
 class App extends Component {
   state = {
@@ -12,10 +13,28 @@ class App extends Component {
 
   //fetching images from unspash api
   inputChangeHandler = event => {
-    console.log(event.target.value);
+    axios
+      .get(
+        'https://api.unsplash.com/search/photos/?page=1&per_page=10&query="food"&client_id=8f23666afddaae8fe13f6efc809cdf8d837787ce77f9aaa91f8f7aa0584ea3f7'
+      )
+      .then(res => {
+        const arr = [];
+        res.data.map(el => {
+          arr.push(el.urls.full);
+        });
+
+        this.setState({ images: arr });
+      })
+      .catch(er => {
+        console.log('er : ');
+        console.log(er);
+      });
   };
 
   render() {
+    {
+      console.log(this.state.images);
+    }
     return (
       <div className="App">
         <Titles />
